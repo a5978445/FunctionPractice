be243d6c2fa5df9999c00cd15981c19f46f0b2e0
fitch <510491354@qq.com>
2018-09-12 10:43:28 +0800

    FindPerfectNumber feature

---
+++ FunctionPractice/Core/FindPerfectNumber.swift
@@ -0,0 +1,104 @@
+//
+//  FindPerfectNumber.swift
+//  FunctionPractice
+//
+//  Created by 李腾芳 on 2018/9/11.
+//  Copyright © 2018年 Mobile Foudation Team. All rights reserved.
+//
+
+import UIKit
+
+struct FindPerfectNumber {
+    struct TraditionalStyle {
+        class PerfectNumberFinder {
+            let number: Int
+            var cache = Dictionary<Int, Int>()
+            
+            init(number: Int) {
+                self.number = number
+            }
+            
+            func isFactor(tagetNumber: Int) -> Bool{
+                return number % tagetNumber == 0
+            }
+            
+            func getFactors() -> Set<Int> {
+                var factors = Set<Int>()
+                for i in 1..<number {
+                    if isFactor(tagetNumber: i) {
+                        factors.insert(i)
+                    }
+                }
+                return factors
+            }
+            
+            func aliquoSum() -> Int {
+                
+
+                
+                if let sum = cache[number] {
+                    return sum
+                } else {
+                    var sum = 0
+                    for i in getFactors() {
+                        sum += i
+                    }
+                    cache[number] = sum
+                    return sum
+                    
+                }
+                
+            }
+            
+            func isPerfect() -> Bool {
+               return aliquoSum() == number
+            }
+            
+            func isDeficient() -> Bool {
+                return aliquoSum() < number
+            }
+            
+            func isAbundant() -> Bool {
+                return aliquoSum() > number
+            }
+        }
+        
+        static func getPerfectNumbers(numbers: [Int]) -> [Int] {
+            
+            var perfectNumbers = [Int]()
+            for i in numbers {
+                if i > 1 {
+                    if PerfectNumberFinder(number: i).isPerfect() {
+                        perfectNumbers.append(i)
+                    }
+                }
+              
+                
+            }
+            return perfectNumbers
+            
+        }
+    }
+    struct FunctionStyle {
+        
+        
+        private  static    func isPerfect(number: Int) -> Bool {
+            let range = 1..<number
+            return range.filter { number % $0 == 0 }
+                .reduce(0, +)
+                == number
+        }
+        
+        
+        static func getPerfectNumbers(numbers: [Int]) -> [Int] {
+            
+          return  numbers
+            .filter { $0 > 1 }
+            .filter { self.isPerfect(number: $0) }
+            
+            
+        }
+    }
+}
+
+
---
+++ FunctionPracticeTests/FindPerfectNumberTests.swift
@@ -0,0 +1,41 @@
+//
+//  FindPerfectNumberTests.swift
+//  FunctionPracticeTests
+//
+//  Created by 李腾芳 on 2018/9/11.
+//  Copyright © 2018年 Mobile Foudation Team. All rights reserved.
+//
+@testable import FunctionPractice
+import XCTest
+
+class FindPerfectNumberTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testFindPerfectNumber_TraditionalStyle() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let range = 0...1000
+        let  sourceNumbers = range.map { $0 }
+        
+        let result0 = FindPerfectNumber.TraditionalStyle.getPerfectNumbers(numbers: sourceNumbers)
+        let result1 = FindPerfectNumber.FunctionStyle.getPerfectNumbers(numbers: sourceNumbers)
+        
+        XCTAssert(result0 == result1)
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
